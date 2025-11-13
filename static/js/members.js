821b1094b82d390c816be0870f6f44d4f6365edd
sebrai <sebbranden@gmail.com>
2025-11-13 13:58:03 +0100

    stuff doesnt work but i dont have time

--- static/js/members.js
+++ static/js/members.js
@@ -1,9 +1,14 @@
 console.log("hello")
 let raw = document.getElementById("data").textContent
 
-
+function pdate(y, m, d) {
+    return `${d}/${m}/${y}`
+}
 function py_to_json(str = raw) {
-    return str.replaceAll("(", "[").replaceAll(")", "]").replaceAll("'", '"');
+    
+    result = str.replaceAll("(", "[").replaceAll(")", "]").replaceAll("'", '"').replaceAll('datetime.date(.*)','pdate(.*)');
+     console.table(JSON.parse(result))
+    return result
 }
 let data = JSON.parse(py_to_json(raw))
 console.table(data)
