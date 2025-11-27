04530ca8440d6ca14a98e50048939605a3e98f0b
sebrai <sebbranden@gmail.com>
2025-11-27 12:45:28 +0100

    made header be created by html

--- static/js/events.js
+++ static/js/events.js
@@ -1,35 +1,4 @@
 datatable = document.getElementById("table")
-let tnid = document.createTextNode("id")
-let tnname = document.createTextNode("title")
-let tdesc = document.createTextNode("description")
-let tscale = document.createTextNode("event scale")
-let tneed = document.createTextNode("members needed")
-let tdone = document.createTextNode("finished")
-
-let eid = document.createElement("th")
-let ename = document.createElement("th")
-let edesc = document.createElement("th")
-let escale = document.createElement("th")
-let eneed = document.createElement("th")
-let eopt = document.createElement("th")
-
-eid.appendChild(tnid)
-ename.appendChild(tnname)
-edesc.appendChild(tdesc)
-escale.appendChild(tscale)
-eneed.appendChild(tneed)
-eopt.appendChild(tdone)
-
-let headrow = document.createElement("tr")
-headrow.appendChild(eid)
-headrow.appendChild(ename)
-headrow.appendChild(edesc)
-headrow.appendChild(escale)
-headrow.appendChild(eneed)
-headrow.appendChild(eopt)
-
-
-table.appendChild(headrow)
 for (let J = 0; J < data.length; J++) {
     const element = data[J];
 
